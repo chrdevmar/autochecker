2e86b2af912a1fe152f83546fd6d32529c830a43
Alaric von Teplitz <alaric2000@gmail.com>
2018-06-30 09:49:18 -0600

    Changed HTML to conform to spec. Moved CSS to its own file. Added ESLint as a dev dependency. Fixed JS files to lint propetly. Replaced certain bits with ES6 syntax. Still needs work though.

--- src/popup.js
+++ src/popup.js
@@ -2,19 +2,19 @@ function generateHeader(operations = []) {
   const container = document.getElementById('autochecker-results');
   const headerElement = document.createElement('h3');
   let headerText;
-  if(operations.length === 1){
+  if (operations.length === 1) {
     headerText = document.createTextNode('1 checkbox detected on this page.');
   } else {
     headerText = document.createTextNode(`${operations.length} checkboxes detected on this page.`);
   }
-  headerElement.appendChild(headerText)
-  headerElement.appendChild(document.createElement('hr'))
-  container.appendChild(headerElement)
+  headerElement.appendChild(headerText);
+  headerElement.appendChild(document.createElement('hr'));
+  container.appendChild(headerElement);
 }
 
 function generateCheckboxInfo(operations = []) {
   const container = document.getElementById('autochecker-results');
-  if(operations.length){
+  if (operations.length) {
     operations.forEach((operation) => {
       // create title for checkbox
       const titleElement = document.createElement('h4');
@@ -32,22 +32,22 @@ function generateCheckboxInfo(operations = []) {
       // now add the message from auto-checker
       const infoElement = document.createElement('p');
       const infoText = document.createTextNode(`The above text was identified as ${operation.category}. The checkbox has been ${operation.action}.`);
-      infoElement.appendChild(infoText)
+      infoElement.appendChild(infoText);
       container.appendChild(infoElement);
-      container.appendChild(document.createElement('hr'))
-    })
+      container.appendChild(document.createElement('hr'));
+    });
   } else {
-    var infoContainer = document.createElement('p');
-    var infoText = document.createTextNode('Autochecker was unable to find any checkboxes.')
+    const infoContainer = document.createElement('p');
+    const infoText = document.createTextNode('Autochecker was unable to find any checkboxes.');
     infoContainer.appendChild(infoText);
     container.appendChild(infoContainer);
   }
 }
 
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, (tabs) => {
-  const url = tabs[0].url;
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+  const { url } = tabs[0];
   chrome.storage.sync.get([url], (data) => {
     generateHeader(data[url]);
     generateCheckboxInfo(data[url]);
-  })
+  });
 });
